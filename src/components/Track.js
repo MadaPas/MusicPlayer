502e877b067481a44d6ec7008ba23bc3aed8ec74
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-04 14:11:09 +0100

    styles

--- src/components/Track.js
+++ src/components/Track.js
@@ -3,7 +3,7 @@ import React from "react";
 const Track = ({ currentTrack, isPlaying }) => {
   return (
     <div className="track-container">
-      <img src={currentTrack.cover} alt={currentTrack.name} className={`${isPlaying?"rotate-song-img":""}`} ></img>
+      <img src={currentTrack.cover} alt={currentTrack.name} className={`${isPlaying?"rotate-track-img":""}`} ></img>
       <h2>{currentTrack.name}</h2>
       <h3>{currentTrack.artist}</h3>
     </div>

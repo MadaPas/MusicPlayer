ddc26f90709b89988e49c33b0279d7ae5ef718b7
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-04 15:36:42 +0100

    almost there

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Copyright &copy; 2020 , MadaPas</p>
+      <p>Copyright &copy; 2021 , MadaPas</p>
     </footer>
   );
 };
--- src/components/Track.js
+++ src/components/Track.js
@@ -3,7 +3,11 @@ import React from "react";
 const Track = ({ currentTrack, isPlaying }) => {
   return (
     <div className="track-container">
-      <img src={currentTrack.cover} alt={currentTrack.name} className={`${isPlaying?"rotate-track-img":""}`} ></img>
+      <img
+        src={currentTrack.cover}
+        alt={currentTrack.name}
+        className={`${isPlaying ? "rotate-track-img" : ""}`}
+      ></img>
       <h2>{currentTrack.name}</h2>
       <h3>{currentTrack.artist}</h3>
     </div>

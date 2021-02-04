cb4a469fc5f09975503adb228668ffb6213b6380
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-04 09:46:29 +0100

    library functionality working for playing songs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import React from "react"
 
-import Song from "./components/Song";
-import Player from "./components/Player";
+import React, { useRef, useState } from "react";
+
+import music from "./music";
 import "./styles/app.scss";
 
+import Track from "./components/Track";
+import Player from "./components/Player";
+import Navigation from "./components/Navigation";
+import Library from "./components/Library";
+
 function App() {
+
+  const [tracks, setTracks] = useState(music());
+  const [isPlaying, setPlaying] = useState(false);
+  const [currentTrack, setCurrentTrack] = useState(tracks[0]);
+ 
+  const [trackInfo, setTrackInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
+
+  const [libraryStatus, setLibraryStatus]=useState(false);
+
+  const audioReference = useRef(null);
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setTrackInfo({ ...trackInfo, currentTime: current, duration: duration });
+  };
   return (
     <div className="App">
-     <Song/>
-     <Player/>
+    <Navigation libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} /> 
+      <Track currentTrack={currentTrack} />
+      <Player
+        trackInfo={trackInfo}
+        setTrackInfo={setTrackInfo}
+        timeUpdateHandler={timeUpdateHandler}
+        audioReference={audioReference}
+        isPlaying={isPlaying}
+        setPlaying={setPlaying}
+        currentTrack={currentTrack}
+      />
+      <Library
+      libraryStatus={libraryStatus}
+        setTracks={setTracks}
+        audioReference={audioReference}
+        tracks={tracks}
+        setCurrentTrack={setCurrentTrack}
+        isPlaying={isPlaying}
+      />
+      <audio
+        onLoadedMetadata={timeUpdateHandler}
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioReference}
+        src={currentTrack.audio}
+      ></audio>
     </div>
   );
 }
